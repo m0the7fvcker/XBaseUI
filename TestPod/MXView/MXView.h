10c594a46c7c4ad750614fb724ca5f47bbd027f6
maRk <7624619@qq.com>
2016-12-01 15:58:57 +0800

    项目初始化
    项目初始化

---
+++ TestPod/MXView/MXView.h
@@ -0,0 +1,15 @@
+//
+//  MXView.h
+//  TestPod
+//
+//  Created by maRk on 16/12/1.
+//  Copyright © 2016年 maRk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MXView : UIView
+
++ (instancetype)getView;
+
+@end
---
+++ TestPod/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestPod
+//
+//  Created by maRk on 16/12/1.
+//  Copyright © 2016年 maRk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
